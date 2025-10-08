ab2f249c699e67d1f75e472d7d1908bff5c44aaa
Henri Fournier <hfournier@intratela.com>
2025-10-08 13:44:38 -0700

    Upgraded to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,77 +1,82 @@
 import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
 import icon from "astro-icon"
 import netlify from "@astrojs/netlify"
 import mdx from "@astrojs/mdx"
 import { remarkReadingTime } from "./remark-reading-time.mjs"
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://henrifournier.dev",
-	output: "static",
-	integrations: [
-		tailwind({
-			applyBaseStyles: false
-		}),
-		icon({
-			include: {
-				devicon: [
-					"alpinejs",
-					"angular",
-					"astro",
-					"bootstrap",
-					"css3",
-					"github",
-					"html5",
-					"javascript",
-					"mongodb",
-					"nestjs",
-					"netlify",
-					"nodejs",
-					"svelte",
-					"tailwindcss",
-					"typescript"
-				],
-				logos: ["stripe"],
-				mdi: [
-					"add-circle-outline",
-					"arrow-up-thin",
-					"book-clock-outline",
-					"chevron-down",
-					"circle-outline",
-					"close",
-					"close-circle",
-					"code-tags",
-					"code-block-tags",
-					"comment",
-					"comment-minus",
-					"comment-plus",
-					"dots-vertical",
-					"email",
-					"emoji-outline",
-					"facebook",
-					"file-document",
-					"file-document-arrow-right",
-					"filter-menu-outline",
-					"format-bold",
-					"format-italic",
-					"format-quote-open",
-					"format-strikethrough-variant",
-					"github",
-					"linkedin",
-					"phone",
-					"rss",
-					"search",
-					"send",
-					"timelapse",
-					"twitter"
-				]
-			}
-		}),
-		mdx()
+  site: "https://henrifournier.dev",
+  output: "static",
+
+  integrations: [
+      icon({
+          include: {
+              devicon: [
+                  "alpinejs",
+                  "angular",
+                  "astro",
+                  "bootstrap",
+                  "css3",
+                  "github",
+                  "html5",
+                  "javascript",
+                  "mongodb",
+                  "nestjs",
+                  "netlify",
+                  "nodejs",
+                  "svelte",
+                  "tailwindcss",
+                  "typescript"
+              ],
+              logos: ["stripe"],
+              mdi: [
+                  "add-circle-outline",
+                  "arrow-up-thin",
+                  "book-clock-outline",
+                  "chevron-down",
+                  "circle-outline",
+                  "close",
+                  "close-circle",
+                  "code-tags",
+                  "code-block-tags",
+                  "comment",
+                  "comment-minus",
+                  "comment-plus",
+                  "dots-vertical",
+                  "email",
+                  "emoji-outline",
+                  "facebook",
+                  "file-document",
+                  "file-document-arrow-right",
+                  "filter-menu-outline",
+                  "format-bold",
+                  "format-italic",
+                  "format-quote-open",
+                  "format-strikethrough-variant",
+                  "github",
+                  "linkedin",
+                  "phone",
+                  "rss",
+                  "search",
+                  "send",
+                  "timelapse",
+                  "twitter"
+              ]
+          }
+      }),
+      mdx()
 	],
-	adapter: netlify(),
-	markdown: {
-		remarkPlugins: [remarkReadingTime]
-	}
+
+  adapter: netlify(),
+
+  markdown: {
+      remarkPlugins: [remarkReadingTime]
+	},
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
