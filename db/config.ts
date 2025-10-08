7e08974de935a339b9bd5630c9c9dfefbd8c80b9
Henri Fournier <hfournier@intratela.com>
2025-10-08 14:16:56 -0700

    Fixed build errors

--- db/config.ts
+++ db/config.ts
@@ -1,20 +1,20 @@
-import { column, defineDb, defineTable, NOW } from "astro:db"
+// import { column, defineDb, defineTable, NOW } from "astro:db"
 
-const Comment = defineTable({
-	columns: {
-		id: column.number({
-			primaryKey: true
-		}),
-		name: column.text({ optional: true }),
-		email: column.text({ optional: true }),
-		text: column.text(),
-		postSlug: column.text(),
-		createdAt: column.date({ default: NOW }),
-		status: column.text({ default: "pending" })
-	},
-	indexes: [{ on: ["postSlug"] }]
-})
+// const Comment = defineTable({
+// 	columns: {
+// 		id: column.number({
+// 			primaryKey: true
+// 		}),
+// 		name: column.text({ optional: true }),
+// 		email: column.text({ optional: true }),
+// 		text: column.text(),
+// 		postSlug: column.text(),
+// 		createdAt: column.date({ default: NOW }),
+// 		status: column.text({ default: "pending" })
+// 	},
+// 	indexes: [{ on: ["postSlug"] }]
+// })
 
-export default defineDb({
-	tables: { Comment }
-})
+// export default defineDb({
+// 	tables: { Comment }
+// })
